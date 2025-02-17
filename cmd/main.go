cb25065de5e28a5080b1f426346cbb5f8effe94a
josiah <josiah@appstack.nyc>
2025-02-17 16:33:16 -0500

    add left and right panel using tview

--- cmd/main.go
+++ cmd/main.go
@@ -20,6 +20,26 @@ func main() {
 		focus:       false,
 	}
 
+	pages := tview.NewPages()
+
+	modal := tview.NewModal().
+		SetText("Do you want to quit the application?").
+		AddButtons([]string{"Quit", "Cancel"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			if buttonLabel == "Quit" {
+				app.Stop()
+			}
+		})
+	
+	// inputField := tview.NewInputField().
+	// 		SetLabel("Enter a number: ").
+	// 		SetPlaceholder("E.g. 1234").
+	// 		SetFieldWidth(10).
+	// 		SetAcceptanceFunc(tview.InputFieldInteger).
+	// 		SetDoneFunc(func(key tcell.Key) {
+	// 			app.Stop()
+	// 		})
+
 	layout := tview.NewFlex().
 		AddItem(tview.NewBox().SetBorder(true).SetTitle("Breakpoints"), 0, 1, false).
 		AddItem(tview.NewBox().SetBorder(true).SetTitle("Debugging"), 0, 3, false)
@@ -28,13 +48,19 @@ func main() {
 		AddItem(layout, 0, 1, false).
 		AddItem(footer.view, footer.fixedSize, footer.proportions, footer.focus)
 
+	pages.AddPage("layout",baseLayout, true, true)
+	pages.AddPage("modal", modal, true, true)
+	// pages.AddPage("input", inputField, true, true)
+	
 	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyEsc {
 			app.Stop()
 		}
 		return event
 	})
-	if err := app.SetRoot(baseLayout, true).EnableMouse(true).Run(); err != nil {
+
+	if err := app.SetRoot(pages, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 }
+
